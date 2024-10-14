79c2064a398bdca5c2ff9f249410ec9895a1dd71
Giovanne Mendonca <giovannemendonca@outlook.com>
2024-10-14 15:26:23 -0300

    init project

---
+++ src/main/java/dev/giovannemendonca/springs3storage/SpringS3StorageApplication.java
@@ -0,0 +1,13 @@
+package dev.giovannemendonca.springs3storage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringS3StorageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringS3StorageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/giovannemendonca/springs3storage/SpringS3StorageApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.giovannemendonca.springs3storage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringS3StorageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
